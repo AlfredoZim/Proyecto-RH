5893d74f8f21f118fc32616faed991742f5c9db3
Alfredo Gómez <71914618+AlfredoZim@users.noreply.github.com>
2022-03-14 06:55:17 -0600

    Add files via upload

---
+++ modales/app.js
@@ -0,0 +1,13 @@
+const registry = document.getElementById("registered");
+const modal_registry = document.getElementById("m-register");
+const close_modal_registry = document.getElementById("close");
+
+
+registry.addEventListener('click', () => {
+    modal_registry.classList.add('show');
+    console.log("hola")
+})
+
+close_modal_registry.addEventListener('click', () => {
+    modal_registry.classList.remove('show');
+})
