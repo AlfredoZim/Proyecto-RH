ac6ab8bba45d23d3cecaac1a1fe8927eb489ed4e
Alfredo Gómez <71914618+AlfredoZim@users.noreply.github.com>
2022-03-17 07:50:34 -0600

    Add files via upload

---
+++ components/header/header.js
@@ -0,0 +1,38 @@
+class show_header extends HTMLElement{
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+        this.innerHTML = `
+        <header>
+        <div class="space-logo">
+            <h2>BankIndra</h2>
+            <h3>An Indra company</h3>
+        </div>
+        <nav>
+            <div class="menu-bars">
+                <button id="slide-menu"><i class="fa-solid fa-bars"></i></button>
+            </div>
+            <div class="left">
+                <a href="#">INICIO</a>
+                <a href="#">ENTREVISTADORES</a>
+                <a href="#">ASPIRANTES</a>
+                <a href="#">EXÁMENES</a>
+                <a href="#">REPORTES</a>
+            </div>
+            <div class="right">
+                <a h ref="#" class="item"><i class="fa-solid fa-right-to-bracket"></i></a>
+            </div>
+        </nav>
+        <div class="responsive-menu" id="menu-r">
+            <a href="#">INICIO</a>
+            <a href="#">ENTREVISTADORES</a>
+            <a href="#">ASPIRANTES</a>
+            <a href="#">EXÁMENES</a>
+            <a href="#">REPORTES</a>
+        </div>
+        </header>`;
+    }
+}
+
+window.customElements.define('my-header',show_header);
