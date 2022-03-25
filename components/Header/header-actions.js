074d36738750f980e9319a777f2656224693084a
Joelandia <joelandia.92@gmail.com>
2022-03-25 16:08:24 -0600

    Delete components/Header directory
    eliminar duplicado

--- components/Header/header-actions.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById("slide-menu");
-const menu_show = document.getElementById("menu-r");
-
-menu.addEventListener("click", () => {
-    menu_show.classList.toggle("show-menu");
-});
