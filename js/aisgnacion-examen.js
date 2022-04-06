bbc301ba95b19f6addfb3cd9fecf9b9e9742c9a6
Alfredo Gómez <71914618+AlfredoZim@users.noreply.github.com>
2022-04-06 12:35:58 -0500

    Delete aisgnacion-examen.js

--- js/aisgnacion-examen.js
+++
@@ -1,6 +0,0 @@
-const n_user = document.getElementById("n-user");
-const s_user = document.getElementById("s-user");
-
-let aspirante = getElemento("aspirante","email","AAA@hotmail.com");
-n_user.innerText = aspirante.nombre;
-s_user.innerText = aspirante.nombre;
