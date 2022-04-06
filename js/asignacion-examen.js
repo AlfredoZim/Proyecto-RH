13908d780105d793371c5175fe392bdb50195655
Alfredo Gómez <71914618+AlfredoZim@users.noreply.github.com>
2022-04-06 12:43:21 -0500

    Add files via upload

--- js/asignacion-examen.js
+++ js/asignacion-examen.js
@@ -1,9 +1,9 @@
-const n_user = document.getElementById("n-user");
-const s_user = document.getElementById("s-user");
-
-let aspirante = getElemento("aspirante","email","AAA@hotmail.com");
-n_user.innerText = aspirante.nombre;
-s_user.innerText = aspirante.nombre;
+//const n_user = document.getElementById("n-user");
+//const s_user = document.getElementById("s-user");
+//
+//let aspirante = getElemento("aspirante","email","AAA@hotmail.com");
+//n_user.innerText = aspirante.nombre;
+//s_user.innerText = aspirante.nombre;
 
 const btn_addexam = document.getElementById("add-exam");
 const select_exam = document.getElementById("select-exam");
